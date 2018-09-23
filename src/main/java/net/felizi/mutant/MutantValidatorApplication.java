c8b3cb5f83a83fda5bf2dda4f7b77af93a13825d
Daniel Felizi <felizi.daniel@gmail.com>
2018-09-23 11:38:47 -0300

    Spring architecture base

---
+++ src/main/java/net/felizi/mutant/MutantValidatorApplication.java
@@ -0,0 +1,12 @@
+package net.felizi.mutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantValidatorApplication.class, args);
+	}
+}
