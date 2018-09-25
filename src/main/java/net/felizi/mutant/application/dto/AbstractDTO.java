826fe99a43c4b9bcbb7f10516c575b94f22b20e8
Daniel Felizi <felizi.daniel@gmail.com>
2018-09-25 10:58:34 -0300

    Abstract DTO for implicit toString

---
+++ src/main/java/net/felizi/mutant/application/dto/AbstractDTO.java
@@ -0,0 +1,9 @@
+package net.felizi.mutant.application.dto;
+
+import java.io.Serializable;
+
+public abstract class AbstractDTO implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  public abstract String toString();
+}
